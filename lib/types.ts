785f4cbe13152fe217b3f0e39f5a7299716b4018
nikhil <nikhilrajpurohitnkp@gmail.com>
2025-08-11 22:22:56 +0530

    added type interface

---
+++ lib/types.ts
@@ -0,0 +1,20 @@
+//creating the interface for my data
+export interface PersonalInfo{
+    name:string,
+    title:string,
+    email:string,
+    image:string,
+}
+
+
+export interface SocialLinks{
+    x:string,
+    github:string,
+    linkedIn:string,
+    email:string,
+}
+
+export interface Education{
+    institution:string,
+    degree:string,
+}
